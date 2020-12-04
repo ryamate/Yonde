96bd685111e5fc96ae61f972c1b642ecad5f1255
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-04 21:23:44 +0900

    closed #2
    - 絵本検索ページ作成
    - scss導入

--- src/search_input.php
+++ src/search_input.php
@@ -4,20 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/css/app.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <title>絵本検索</title>
 </head>
 
 <body>
-    <h1>絵本の検索</h1>
-    <p>まずは絵本のタイトル、作家名、出版社名などを入力して検索してください。</p>
-    <p>例えば… パンやのろくちゃん や はらぺこあおむし や えんとつ町のプペル など。</p>
-    <form action="search_api.php" method="POST">
-        <div>
-            <label for="search">検索</label>
-            <input type="text" id="search" name="search">
-            <button type="submit">検索する</button>
-        </div>
-    </form>
+    <div class="container">
+        <h1>絵本の検索</h1>
+        <p>まずは絵本のタイトル、作家名、出版社名などを入力して検索してください。</p>
+        <p>例えば… パンやのろくちゃん や はらぺこあおむし や おしりたんてい など。</p>
+        <form action="search_api.php" method="POST">
+            <div>
+                <label for="search">検索</label>
+                <input type="text" id="search" name="search">
+                <button type="submit" class="btn btn-teal1 shadow mt-2 mb-2">検索する</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 
--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -18,6 +18,7 @@ class ComposerStaticInitadc49d6f1f77bc3ccc8f5fe39935cd72
             'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
+            'ScssPhp\\ScssPhp\\' => 16,
         ),
         'P' => 
         array (
@@ -45,6 +46,10 @@ class ComposerStaticInitadc49d6f1f77bc3ccc8f5fe39935cd72
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'ScssPhp\\ScssPhp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/scssphp/scssphp/src',
         ),
         'PhpOption\\' => 
         array (
