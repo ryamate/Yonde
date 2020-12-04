c1f208804b62365374ca3247dbaebab6f19c4b74
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-04 21:23:44 +0900

    closed #2
    - 絵本検索ページ作成
    - scss導入

---
+++ src/tmp/sample.php
@@ -0,0 +1,66 @@
+ <!-- ポイント３つ -->
+ <div style="margin-top: 74px;">
+     <div class="container d-none d-md-block mt-4">
+         <div class="card-deck row">
+             <div class="card border-0">
+                 <a href=""><img src="image/point_1.png" width="100%" alt=""></a>
+                 <div class="card-body">
+                     <h4 class="card-title"><b>図解中心の動画で学ぶ</b></h4>
+                     <p class="card-text">図解を中心にした動画解説で、イメージをつかみながら理解できます。<br>
+                         動画は1本20分以内の長さで、集中力を切らさずに学習を進めていけます。
+                     </p>
+                 </div>
+             </div>
+             <div class="card border-0">
+                 <a href=""><img src="image/point_2.png" width="100%" alt=""></a>
+                 <div class="card-body">
+                     <h4 class="card-title"><b>実務で必要スキルを網羅</b></h4>
+                     <p class="card-text">Web系のサーバーサイドエンジニアとして実務で必要なスキルを網羅しました。<br>
+                         ロードマップに沿って学習していくので、何を学習すればよいか迷うことはもうありません。
+                     </p>
+                 </div>
+             </div>
+             <div class="card border-0">
+                 <a href=""><img src="image/point_3.png" width="100%" alt=""></a>
+                 <div class="card-body">
+                     <h4 class="card-title"><b>実務レベルの演習</b></h4>
+                     <p class="card-text">実務で実際にあるような課題を演習として取り組みます。<br>
+                         経験年数2〜3年のエンジニアレベルの「実務レベルのスキル」を習得します。
+                     </p>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- 幅狭い時 -->
+ <div class="container d-block d-md-none mt-4">
+     <div class="card-deck row">
+         <div class="card border-0">
+             <a href=""><img src="image/point_1.png" width="100%" alt=""></a>
+             <div class="card-body">
+                 <h5 class="card-title"><b>図解中心の動画で学ぶ</b></h5>
+                 <p class="card-text">図解を中心にした動画解説で、イメージをつかみながら理解できます。<br>
+                     動画は1本20分以内の長さで、集中力を切らさずに学習を進めていけます。
+                 </p>
+             </div>
+         </div>
+         <div class="card border-0">
+             <a href=""><img src="image/point_2.png" width="100%" alt=""></a>
+             <div class="card-body">
+                 <h5 class="card-title"><b>実務で必要スキルを網羅</b></h5>
+                 <p class="card-text">Web系のサーバーサイドエンジニアとして実務で必要なスキルを網羅しました。<br>
+                     ロードマップに沿って学習していくので、何を学習すればよいか迷うことはもうありません。
+                 </p>
+             </div>
+         </div>
+         <div class="card border-0">
+             <a href=""><img src="image/point_3.png" width="100%" alt=""></a>
+             <div class="card-body">
+                 <h5 class="card-title"><b>実務レベルの演習</b></h5>
+                 <p class="card-text">実務で実際にあるような課題を演習として取り組みます。<br>
+                     経験年数2〜3年のエンジニアレベルの「実務レベルのスキル」を習得します。
+                 </p>
+             </div>
+         </div>
+     </div>
+ </div>
