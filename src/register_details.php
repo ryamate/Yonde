92f7c0b5baa9315553d4b914e819bf664a56f412
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-05 18:06:15 +0900

    navber作成、リファクタリング（html,php切り分け）
    - 20201205作業
    - 本棚に登録機能これから

---
+++ src/register_details.php
@@ -0,0 +1,5 @@
+<?php
+
+$title = '詳細登録';
+$content = __DIR__ . '/views/register_details.php';
+include __DIR__ . '/views/layout.php';
---
+++ src/views/layout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/css/app.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+    <nav class="fixed-top navbar navbar-expand-md shadow-sm" style="background: rgba(255,255,255,0.9);">
+        <h1 class="h2">
+            <a class="text-teal1 text-decoration-none" href="search_new.php"><img src="/image/package_design.png" alt="yonde" height="45">よんで</a>
+        </h1>
+    </nav>
+    <header>
+
+    </header>
+    <main>
+        <div class="container" style="margin-top: 90px;">
+            <?php include $content; ?>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
