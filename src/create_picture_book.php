199921a3b23d0b381615af8d8ee4404098b6a4c0
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-08 21:57:20 +0900

    本棚登録、PHPからMySQLのテーブルにデータ追加処理

--- src/create_picture_book.php
+++ src/create_picture_book.php
@@ -6,13 +6,13 @@ function createPictureBook($link, $picture_book)
 {
     $sql = <<<EOT
 INSERT INTO picture_books (
-    picture_book_id,
+    isbn_13,
     title,
     authors,
     published_date,
     thumbnail_uri
 )VALUES (
-    "{$picture_book['picture_book_id']}",
+    "{$picture_book['isbn_13']}",
     "{$picture_book['title']}",
     "{$picture_book['authors']}",
     "{$picture_book['published_date']}",
@@ -21,26 +21,27 @@ INSERT INTO picture_books (
 EOT;
     $result = mysqli_query($link, $sql);
     if ($result) {
-        echo '登録が完了しました' . PHP_EOL;
+        // echo '登録が完了しました' . PHP_EOL;
     } else {
         echo 'Error: データの追加に失敗しました' . PHP_EOL;
         echo 'Debugging Error:' . mysqli_error($link) . PHP_EOL;
     }
 }
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // $picture_book = $_POST;
-    $picture_book = [
-        'picture_book_id' => (int)$_POST['picture_book_id'],
-        'title' => $_POST['title'],
-        'authors' => $_POST['authors'],
-        'published_date' => $_POST['published_date'],
-        'thumbnail_uri' => $_POST['thumbnail_uri']
-    ];
-    var_dump($picture_book);
+    $picture_book = $_POST;
+    // $picture_book = [
+    //     'isbn_13' => $_POST['isbn_13'],
+    //     'title' => $_POST['title'],
+    //     'authors' => $_POST['authors'],
+    //     'published_date' => $_POST['published_date'],
+    //     'thumbnail_uri' => $_POST['thumbnail_uri']
+    // ];
     // バリデーションする
     $link = dbConnect();
     createPictureBook($link, $picture_book);
     mysqli_close($link);
     // header("Location: register_details.php");
+    include __DIR__ . '/register_details.php';
 }
