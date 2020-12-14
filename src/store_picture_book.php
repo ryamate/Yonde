09db265b7a317b6a65d3db1ca27d99eab2b8ae99
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-14 18:18:29 +0900

    ER図案の整理、picture_books_tableの修正
    - picture_books_tableの修正にともなった、各ファイル修正

--- src/store_picture_book.php
+++ src/store_picture_book.php
@@ -2,16 +2,27 @@
 
 require_once __DIR__ . '/lib/mysqli.php';
 
+function getPictureBookId($link, $stored_picture_book)
+{
+    $sql = "SELECT id FROM picture_books where isbn_13 = '{$stored_picture_book['isbn_13']}'";
+    $results = mysqli_query($link, $sql);
+    $picture_book_id = mysqli_fetch_assoc($results);
+
+    mysqli_free_result($results);
+
+    return $picture_book_id;
+}
+
 function storePictureBook($link, $stored_picture_book)
 {
     $sql = <<<EOT
 INSERT INTO stored_picture_books (
-    isbn_13,
+    picture_book_id,
     five_star_rating,
     read_status,
     review
 )VALUES (
-    "{$stored_picture_book['isbn_13']}",
+    "{$stored_picture_book['id']}",
     "{$stored_picture_book['five_star_rating']}",
     "{$stored_picture_book['read_status']}",
     "{$stored_picture_book['review']}"
@@ -43,6 +54,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'review' => $_POST['review'],
     ];
     // バリデーションする
+    $link = dbConnect();
+    $picture_book_id = getPictureBookId($link, $stored_picture_book);
+    $stored_picture_book = array_merge($picture_book_id, $stored_picture_book);
+
     $link = dbConnect();
     storePictureBook($link, $stored_picture_book);
     mysqli_close($link);
