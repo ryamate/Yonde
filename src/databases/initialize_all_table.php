5b804933f5e0b1a4b924e79571dd9935bdf12e54
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-15 06:36:11 +0900

    ER図 微修正

--- src/databases/initialize_all_table.php
+++ src/databases/initialize_all_table.php
@@ -82,6 +82,7 @@ function dropReadRecordsTable($link)
         echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
     }
 }
+
 function dropChildrenTable($link)
 {
     $dropTableSql = 'DROP TABLE IF EXISTS children;';
@@ -119,37 +120,14 @@ function createPictureBooksTable($link)
     }
 }
 
-function createStoredPictureBooksTable($link)
-{
-    $createTableSql = <<<EOT
-    CREATE TABLE stored_picture_books (
-        id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        picture_book_id INTEGER UNIQUE NOT NULL,
-        five_star_rating INTEGER,
-        read_status VARCHAR(100),
-        review VARCHAR(1000),
-        created_at TIMESTAMP NULL,
-        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (picture_book_id) REFERENCES picture_books(id)
-    ) DEFAULT CHARACTER SET=utf8mb4;
-    EOT;
-    $result = mysqli_query($link, $createTableSql);
-    if ($result) {
-        echo 'テーブルを作成しました' . PHP_EOL . PHP_EOL;
-    } else {
-        echo 'Error: テーブルの作成に失敗しました' . PHP_EOL;
-        echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
-    }
-}
-
 function createUsersTable($link)
 {
     $createTableSql = <<<EOT
     CREATE TABLE users (
         id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        user_name VARCHAR(100),
-        email VARCHAR(100),
-        password VARCHAR(100),
+        user_name VARCHAR(255),
+        email VARCHAR(128) UNIQUE,
+        password VARCHAR(255),
         introduction VARCHAR(1000),
         user_image_path VARCHAR(1000),
         created_at TIMESTAMP NULL,
@@ -185,6 +163,53 @@ function createFamiliesTable($link)
     }
 }
 
+function createChildrenTable($link)
+{
+    $createTableSql = <<<EOT
+    CREATE TABLE children (
+        id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        family_id INTEGER UNIQUE NOT NULL,
+        child_name VARCHAR(1000),
+        child_birthday TIMESTAMP NULL,
+        created_at TIMESTAMP NULL,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (family_id) REFERENCES families(id)
+    ) DEFAULT CHARACTER SET=utf8mb4;
+    EOT;
+    $result = mysqli_query($link, $createTableSql);
+    if ($result) {
+        echo 'テーブルを作成しました' . PHP_EOL . PHP_EOL;
+    } else {
+        echo 'Error: テーブルの作成に失敗しました' . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+    }
+}
+
+function createStoredPictureBooksTable($link)
+{
+    $createTableSql = <<<EOT
+    CREATE TABLE stored_picture_books (
+        id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        picture_book_id INTEGER UNIQUE NOT NULL,
+        family_id INTEGER NOT NULL,
+        five_star_rating INTEGER,
+        read_status VARCHAR(100),
+        review VARCHAR(1000),
+        created_at TIMESTAMP NULL,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (picture_book_id) REFERENCES picture_books(id),
+        FOREIGN KEY (family_id) REFERENCES families(id)
+    ) DEFAULT CHARACTER SET=utf8mb4;
+    EOT;
+    $result = mysqli_query($link, $createTableSql);
+    if ($result) {
+        echo 'テーブルを作成しました' . PHP_EOL . PHP_EOL;
+    } else {
+        echo 'Error: テーブルの作成に失敗しました' . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+    }
+}
+
 function createReadRecordsTable($link)
 {
     $createTableSql = <<<EOT
@@ -209,42 +234,21 @@ function createReadRecordsTable($link)
     }
 }
 
-function createChildrenTable($link)
-{
-    $createTableSql = <<<EOT
-    CREATE TABLE children (
-        id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        family_id INTEGER UNIQUE NOT NULL,
-        child_name VARCHAR(1000),
-        child_birthday TIMESTAMP NULL,
-        created_at TIMESTAMP NULL,
-        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (family_id) REFERENCES families(id)
-    ) DEFAULT CHARACTER SET=utf8mb4;
-    EOT;
-    $result = mysqli_query($link, $createTableSql);
-    if ($result) {
-        echo 'テーブルを作成しました' . PHP_EOL . PHP_EOL;
-    } else {
-        echo 'Error: テーブルの作成に失敗しました' . PHP_EOL;
-        echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
-    }
-}
 
 
 $link = dbConnect();
-dropChildrenTable($link);
 dropReadRecordsTable($link);
+dropStoredPictureBooksTable($link);
+dropChildrenTable($link);
 dropFamiliesTable($link);
 dropUsersTable($link);
-dropStoredPictureBooksTable($link);
 dropPictureBooksTable($link);
 
 createPictureBooksTable($link);
-createStoredPictureBooksTable($link);
 createUsersTable($link);
 createFamiliesTable($link);
-createReadRecordsTable($link);
 createChildrenTable($link);
+createStoredPictureBooksTable($link);
+createReadRecordsTable($link);
 
 mysqli_close($link);
