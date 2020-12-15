a65b52d347c48411535f34e2b4cce3701617f036
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-15 21:32:40 +0900

    メールアドレズ、パスワードでの会員登録、ログインのHTML、PHP実装

---
+++ src/views/input_login.php
@@ -0,0 +1,14 @@
+    <h1>ログイン</h1>
+    <form action="log_in.php" method="post">
+        <div>
+            <label for="email">email</label>
+            <input type="email" name="email">
+        </div>
+        <div>
+            <label for="password">password</label>
+            <input type="password" name="password">
+        </div>
+        <div>
+            <button type="submit">ログイン</button>
+        </div>
+    </form>
