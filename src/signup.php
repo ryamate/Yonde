1b4cb59bd6c29adb25961f1ffa5bcfedee7e55b9
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-19 07:24:43 +0900

    feat: 確認画面から戻って修正する機能追加 他
    - 画像アップロード機能追加中
    - style: トップページの微修正
    - トップページの画像の差し替え

--- src/signup.php
+++ src/signup.php
@@ -3,14 +3,6 @@
 require_once __DIR__ . '/lib/escape.php';
 require_once __DIR__ . '/lib/mysqli.php';
 
-session_start();
-//ログイン済みの場合
-if (isset($_SESSION['EMAIL'])) {
-    echo 'ようこそ' .  escape($_SESSION['EMAIL']) . "さん<br>";
-    echo "<a href='/logout.php'>ログアウトはこちら。</a>";
-    exit;
-}
-
 $user = [
     'email' => '',
     'user_name' => '',
@@ -18,6 +10,26 @@ $user = [
 ];
 
 $errors = [];
+
+session_start();
+//ログイン済みの場合
+// if (isset($_SESSION['user_name'])) {
+//     echo 'ようこそ' .  escape($_SESSION['user_name']) . "さん<br>";
+//     echo "<a href='/logout.php'>ログアウトはこちら。</a>";
+//     exit;
+// }
+
+if (!isset($_REQUEST['action'])) {
+    $_REQUEST = ['action' => ''];
+}
+if ($_REQUEST['action'] === 'rewrite' && isset($_SESSION['join'])) {
+    $_POST = $_SESSION['join'];
+    $user = [
+        'email' => $_POST['email'],
+        'user_name' => $_POST['user_name'],
+        'password' => $_POST['password']
+    ];
+}
 
 $title = 'よんで-Yonde-新規登録';
 $content = __DIR__ . '/views/signup.php';
--- src/views/signup.php
+++ src/views/signup.php
@@ -2,30 +2,40 @@
     <div class="container" style="margin-top: 90px;">
         <h3>「よんで」の新規登録</h3>
         <h4>　ようこそ、「よんで」へ。</h4>
-        <form action="create_user.php" method="POST">
-            <?php if (count($errors)) : ?>
-                <div class="text-danger">
-                    <ul>
-                        <?php foreach ($errors as $error) : ?>
-                            <li><?= $error; ?></li>
-                        <?php endforeach; ?>
-                    </ul>
+        <div class="card container mt-4 p-4">
+            <form action="validate_signup.php" method="POST" enctype="multipart/form-data">
+                <p>次のフォームに必要事項をご記入願います。</p>
+                <?php if (count($errors)) : ?>
+                    <div class="text-danger">
+                        <ul>
+                            <?php foreach ($errors as $error) : ?>
+                                <li><?= $error; ?></li>
+                            <?php endforeach; ?>
+                            <li>恐れ入りますが、画像を改めて選択願います。</li>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input class="form-control" type="email" id="email" name="email" value="<?= escape($user['email']); ?>">
                 </div>
-            <?php endif; ?>
-            <div class="form-group">
-                <label for="email">メールアドレス</label>
-                <input class="form-control" type="email" id="email" name="email" value="<?= $user['email']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="user_name">よんでID</label>
-                <input class="form-control" type="user_name" id="user_name" name="user_name" value="<?= $user['user_name']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="password">パスワード</label>
-                <input class="form-control" type="password" id="password" name="password" value="<?= $user['password']; ?>">
-                <p class="text-dark">※パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
-            </div>
-            <button type="submit" class="btn bg-warning text-decoration-none text-white">新規登録する</button>
-        </form>
+                <div class="form-group">
+                    <label for="user_name">よんでID</label>
+                    <input class="form-control" type="user_name" id="user_name" name="user_name" value="<?= escape($user['user_name']); ?>">
+                </div>
+                <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input class="form-control" type="password" id="password" name="password" value="<?= escape($user['password']); ?>">
+                    <p class="text-dark">※パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
+                </div>
+                <div class="form-group">
+                    <label for="image">プロフィール画像</label><br>
+                    <input type="file" name="image" size="35" value="test" />
+                </div>
+                <button type="submit" class="btn bg-warning text-decoration-none text-white">新規登録する</button>
+            </form>
+        </div>
+    </div>
+
     </div>
 </main>
