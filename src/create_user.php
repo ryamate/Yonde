9b84e9495787f53a80636e7685a5274c12449bf7
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-19 15:46:43 +0900

    [feat]新規登録の際のプロフィール画像アップロード機能追加

--- src/create_user.php
+++ src/create_user.php
@@ -1,28 +1,6 @@
 <?php
-
+session_start();
 require_once __DIR__ . '/lib/mysqli.php';
-
-function validateUser($user)
-{
-    $errors = [];
-
-    if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = 'メールアドレス：入力された値が正しくありません。';
-    }
-
-    if (!strlen($user['user_name'])) {
-        $errors['user_name'] = 'よんでID：未入力です。';
-    } elseif (strlen($user['user_name']) > 16) {
-        $errors['user_name'] = 'よんでID：16文字以内 で入力願います。';
-    }
-
-    if (!preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i', $user['password'])) {
-        $errors['password'] =  'パスワード：半角英数字をそれぞれ1文字以上含んだ8文字以上で設定願います。';
-    }
-
-    return $errors;
-}
-
 
 function createUser($link, $user)
 {
@@ -53,7 +31,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'user_name' => $_POST['user_name'],
         'password' => $_POST['password']
     ];
-    $errors = validateUser($user);
     if (!count($errors)) {
         $user['password'] = password_hash($user['password'], PASSWORD_DEFAULT);
         $link = dbConnect();
@@ -64,5 +41,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 $title = 'よんで-Yonde-新規登録';
-$content = __DIR__ . '/views/signup.php';
+$content = __DIR__ . '/views/create_user.php';
 include __DIR__ . '/views/layout_before_login.php';
--- src/views/signup.php
+++ src/views/signup.php
@@ -5,32 +5,49 @@
         <div class="card container mt-4 p-4">
             <form action="validate_signup.php" method="POST" enctype="multipart/form-data">
                 <p>次のフォームに必要事項をご記入願います。</p>
-                <?php if (count($errors)) : ?>
-                    <div class="text-danger">
-                        <ul>
-                            <?php foreach ($errors as $error) : ?>
-                                <li><?= $error; ?></li>
-                            <?php endforeach; ?>
-                            <li>恐れ入りますが、画像を改めて選択願います。</li>
-                        </ul>
-                    </div>
-                <?php endif; ?>
                 <div class="form-group">
                     <label for="email">メールアドレス</label>
                     <input class="form-control" type="email" id="email" name="email" value="<?= escape($user['email']); ?>">
                 </div>
+                <?php if (isset($errors['email'])) : ?>
+                    <div class="text-danger">
+                        <p><?= $errors['email'] ?></p>
+                    </div>
+                <?php endif; ?>
                 <div class="form-group">
                     <label for="user_name">よんでID</label>
                     <input class="form-control" type="user_name" id="user_name" name="user_name" value="<?= escape($user['user_name']); ?>">
                 </div>
+                <?php if (isset($errors['user_name'])) : ?>
+                    <div class="text-danger">
+                        <p><?= $errors['user_name'] ?></p>
+                    </div>
+                <?php endif; ?>
                 <div class="form-group">
                     <label for="password">パスワード</label>
                     <input class="form-control" type="password" id="password" name="password" value="<?= escape($user['password']); ?>">
-                    <p class="text-dark">※パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
+                    <?php if (isset($errors['password'])) : ?>
+                        <div class="text-danger">
+                            <p><?= $errors['password'] ?></p>
+                        </div>
+                    <?php else : ?>
+                        <p class="text-dark">* パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
+                    <?php endif; ?>
                 </div>
+
                 <div class="form-group">
                     <label for="image">プロフィール画像</label><br>
                     <input type="file" name="image" size="35" value="test" />
+                    <div class="text-danger">
+                        <?php if (isset($errors['image'])) : ?>
+                            <?php if ($errors['image'] === 'type') : ?>
+                                <p><?= $errors['image'] ?></p>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                        <?php if (!empty($file_name) || isset($_SESSION['join']['image'])) : ?>
+                            <p>* 恐れ入りますが、画像を改めて選択願います。</p>
+                        <?php endif; ?>
+                    </div>
                 </div>
                 <button type="submit" class="btn bg-warning text-decoration-none text-white">新規登録する</button>
             </form>
