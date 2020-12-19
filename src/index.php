af76b018011dc4804ea21bc71f589a5a6e793720
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-19 15:46:43 +0900

    [feat]新規登録の際のプロフィール画像アップロード機能追加

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+$_SESSION = array();
+session_destroy();
+var_export($_SESSION);
 
 $title = 'よんで-Yonde-';
 $content = __DIR__ . '/views/index.php';
--- src/validate_signup.php
+++ src/validate_signup.php
@@ -3,29 +3,28 @@
 require_once __DIR__ . '/lib/escape.php';
 require_once __DIR__ . '/lib/mysqli.php';
 
-function validateUser($user)
+function validateUser($user, $file_name)
 {
     $errors = [];
 
     if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = 'メールアドレス：入力された値が正しくありません。';
+        $errors['email'] = '*メールアドレス：入力された値が正しくありません。';
     }
 
     if (!strlen($user['user_name'])) {
-        $errors['user_name'] = 'よんでID：未入力です。';
+        $errors['user_name'] = '*よんでID：入力願います。';
     } elseif (strlen($user['user_name']) > 16) {
-        $errors['user_name'] = 'よんでID：16文字以内 で入力願います。';
+        $errors['user_name'] = '*よんでID：16文字以内 で入力願います。';
     }
 
     if (!preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i', $user['password'])) {
-        $errors['password'] =  'パスワード：半角英数字をそれぞれ1文字以上含んだ8文字以上で設定願います。';
+        $errors['password'] =  '*パスワード：半角英数字をそれぞれ1文字以上含んだ8文字以上で設定願います。';
     }
 
-    $file_name = $_FILES['image']['name'];
     if (!empty($file_name)) {
         $ext = substr($file_name, -3);
         if ($ext !== 'gif' && $ext !== 'jpg' && $ext !== 'png') {
-            $errors['image'] = 'プロフィール画像：「.gif」または「.jpg」「.png」のファイルをアップロード願います。';
+            $errors['image'] = '* プロフィール画像：「.gif」または「.jpg」「.png」のファイルをアップロード願います。';
         }
     }
 
@@ -38,14 +37,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'user_name' => $_POST['user_name'],
         'password' => $_POST['password']
     ];
-    $errors = validateUser($user);
+    $file_name = $_FILES['image']['name'];
+    $errors = validateUser($user, $file_name);
+    var_export($file_name);
     if (!count($errors)) {
         session_start();
-        $image = date('YmdHis') . $_FILES['image']['name'];
-        //DBできたら $image = $_FILES['id']['image']['name'];
-        move_uploaded_file($_FILES['image']['tmp_name'], __DIR__ . '/images/user_picture/' . $image);
         $_SESSION['join'] = $_POST;
-        $_SESSION['join']['image'] = $image;
+        if (!empty($file_name)) {
+            $image = date('YmdHis') . $_FILES['image']['name'];
+            //DB接続できたら $image = $_FILES['id']['image']['name'];
+            move_uploaded_file($_FILES['image']['tmp_name'], __DIR__ . '/images/user_picture/' . $image);
+            $_SESSION['join']['image'] = $image;
+        }
         header("Location: check_signup.php");
         exit();
     }
