d4bed926280099ad573ef77b5b09106e5d1f3ee1
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-21 06:10:52 +0900

    [feat]ログイン機能実装 closed #12
    - 「次回からは自動的にログインする」というチェックボタンをログイン画面に表示しているが、IDのみを記憶させる機能
    - ファイル名整理
    - ログイン後のユーザー画面表示テスト実施

--- src/login.php
+++ src/login.php
@@ -3,39 +3,26 @@
 require_once __DIR__ . '/lib/escape.php';
 require_once __DIR__ . '/lib/mysqli.php';
 
-function getEmail($link, $entered_email)
-{
-    $sql = "SELECT * from users where email = '$entered_email'";
-    $result = mysqli_query($link, $sql);
-    $row = mysqli_fetch_assoc($result);
+$user = [
+    'email' => '',
+    'user_name' => '',
+    'password' => ''
+];
 
-    mysqli_free_result($result);
+$errors = [];
 
-    return $row;
+if (isset($_COOKIE['user_name'])) {
+    $user['user_name'] = $_COOKIE['user_name'];
 }
 
 session_start();
-//POSTのvalidate
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    echo '入力された値が不正です。';
-    return false;
-}
-//DB内でPOSTされたメールアドレスを検索
-$entered_email = $_POST['email'];
-$link = dbConnect();
-$row = getEmail($link, $entered_email);
+//ログイン済みの場合
+// if (isset($_SESSION['EMAIL'])) {
+//     echo 'ようこそ' .  escape($_SESSION['EMAIL']) . "さん<br>";
+//     echo "<a href='/logout.php'>ログアウトはこちら。</a>";
+//     exit;
+// }
 
-//emailがDB内に存在しているか確認
-if (!isset($row['email'])) {
-    echo 'メールアドレス又はパスワードが間違っています。';
-    return false;
-}
-//パスワード確認後sessionにメールアドレスを渡す
-if (password_verify($_POST['password'], $row['password'])) {
-    session_regenerate_id(true); //session_idを新しく生成し、置き換える
-    $_SESSION['EMAIL'] = $row['email'];
-    echo 'ログインしました';
-} else {
-    echo 'メールアドレス又はパスワードが間違っています。';
-    return false;
-}
+$title = 'よんで-Yonde-ログイン';
+$content = __DIR__ . '/views/login.php';
+include __DIR__ . '/views/layout_before_login.php';
