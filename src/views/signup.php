8223365daab5477ceee157066cdc3b18610a88d2
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-22 06:42:36 +0900

    [feat]テストユーザーログイン機能 closed #14 closed #20
    - ログインページで一般ログインと並列してテストユーザーログインのボタンを配置
    - 新規登録ページでフォームの下に配置
    - [feat]テーブル初期化の際のテストユーザー作成機能 #20
    - ログイン時にテーブルのテストユーザー情報でログインする

--- src/views/signup.php
+++ src/views/signup.php
@@ -52,6 +52,12 @@
                 <button type="submit" class="btn bg-warning text-decoration-none text-white">新規登録する</button>
             </form>
         </div>
+        <div class="card container mt-4 p-4">
+            <form action="validate_login.php?action=test_user_login" method="POST" enctype="multipart/form-data">
+                <p>* 新規登録せずに機能を試したい方はこちら</p>
+                <button type="submit" class="btn bg-white btn-outline-teal1 text-decoration-none text-teal1">テストユーザーログイン</button>
+            </form>
+        </div>
     </div>
 
     </div>
