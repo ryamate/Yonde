9045b7ab80d5f0d2562c254e4d15fbcc7f3eb2f3
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-24 22:59:03 +0900

    [feat]ユーザーごとのページを表示する機能 closed #22
    - ER図修正
    - テーブル初期化ファイル修正
    - テーブルに合わせて、PHPからMySQLへの接続修正

--- src/views/signup.php
+++ src/views/signup.php
@@ -6,7 +6,7 @@
             <form action="validate_signup.php" method="POST" enctype="multipart/form-data">
                 <p>次のフォームに必要事項をご記入願います。</p>
                 <div class="form-group">
-                    <label for="email">メールアドレス</label>
+                    <label for="email">メールアドレス <span class="badge bg-warning text-white">必須</span></label></label>
                     <input class="form-control" type="email" id="email" name="email" value="<?= escape($user['email']); ?>">
                 </div>
                 <?php if (isset($errors['email'])) : ?>
@@ -15,7 +15,7 @@
                     </div>
                 <?php endif; ?>
                 <div class="form-group">
-                    <label for="user_name">よんでID</label>
+                    <label for="user_name">よんでID <span class="badge bg-warning text-white">必須</span></label></label>
                     <input class="form-control" type="user_name" id="user_name" name="user_name" value="<?= escape($user['user_name']); ?>">
                 </div>
                 <?php if (isset($errors['user_name'])) : ?>
@@ -24,19 +24,19 @@
                     </div>
                 <?php endif; ?>
                 <div class="form-group">
-                    <label for="password">パスワード</label>
+                    <label for="password">パスワード <span class="badge bg-warning text-white">必須</span></label></label>
                     <input class="form-control" type="password" id="password" name="password" value="<?= escape($user['password']); ?>">
                     <?php if (isset($errors['password'])) : ?>
                         <div class="text-danger">
                             <p><?= $errors['password'] ?></p>
                         </div>
                     <?php else : ?>
-                        <p class="text-dark">* パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
+                        <p class="text-dark small">* パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
                     <?php endif; ?>
                 </div>
 
                 <div class="form-group">
-                    <label for="image">プロフィール画像</label><br>
+                    <label for="image">プロフィール画像 <span class="badge bg-light">任意</span></label></label><br>
                     <input type="file" name="image" size="35" value="test" />
                     <div class="text-danger">
                         <?php if (isset($errors['image'])) : ?>
