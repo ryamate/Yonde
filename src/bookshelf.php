5b745cc777c7ca02d9370f8d32b4fe98486cba8f
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-27 11:23:36 +0900

    [refactor]データベース操作についてPDOに置き換える closed #32
    - dbc.php に、データベース関連の関数を集約

--- src/bookshelf.php
+++ src/bookshelf.php
@@ -5,48 +5,24 @@ if (!isset($_SESSION)) {
 }
 
 require_once __DIR__ . '/lib/escape.php';
-require_once __DIR__ . '/lib/mysqli.php';
-
-function getUser($link, $user)
-{
-    $sql = "SELECT * FROM users WHERE user_name = '{$user['user_name']}' ";
-    $result = mysqli_query($link, $sql);
-    $login_user = mysqli_fetch_assoc($result);
-
-    mysqli_free_result($result);
-
-    return $login_user;
-}
-
-function listStoredPictureBooks($link, $login_user)
-{
-
-    $stored_picture_books = [];
-    $sql = "SELECT s.id, s.picture_book_id, s.five_star_rating, s.read_status, s.possession, s.summary, p.title, p.authors, p.thumbnail_uri, p.published_date FROM stored_picture_books s JOIN picture_books p ON s.picture_book_id = p.id WHERE user_id = '{$login_user['id']}'";
-    $results = mysqli_query($link, $sql);
-
-    while ($stored_picture_book = mysqli_fetch_assoc($results)) {
-        $stored_picture_books[] = $stored_picture_book;
-    }
-
-    mysqli_free_result($results);
-
-    return $stored_picture_books;
-}
+require_once __DIR__ . '/lib/db_connect.php';
 
 if (isset($_SESSION['user_name']) && $_SESSION['time'] + 60 * 60 > time()) {
     $user = [
         'user_name' => $_SESSION['user_name'],
     ];
-    $link = dbConnect();
-    $login_user = getUser($link, $user);
+    $dbc = new Dbc;
+    $login_user = $dbc->getLoginUser($user);
     $_SESSION = $login_user;
     $_SESSION['time'] = time();
-    $stored_picture_books = listStoredPictureBooks($link, $login_user);
+
+    $stored_picture_books = $dbc->listStoredPictureBooks($login_user);
 } else {
     header('Location: login.php');
     exit;
 }
+
+// var_export($stored_picture_books);
 
 $title = 'よんで-Yonde-絵本棚';
 $content = __DIR__ . '/views/bookshelf.php';
