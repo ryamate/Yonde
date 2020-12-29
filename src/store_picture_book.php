3e6a7445a066f5b791d4e43bfc3d452de5dcaa13
ryamate <iiiiiii.satenanbon@gmail.com>
2020-12-29 19:48:14 +0900

    [bug][feat] 検索結果ページで登録済みの本は、登録済みと表示させる closed #39
    - 条件分岐で、登録済 or 未登録 を分岐させた

--- src/store_picture_book.php
+++ src/store_picture_book.php
@@ -2,9 +2,13 @@
 
 session_start();
 
+require_once __DIR__ . '/lib/escape.php';
 require_once __DIR__ . '/lib/db_connect.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if (!isset($_REQUEST['action'])) {
+    $_REQUEST = ['action' => ''];
+}
+if ($_REQUEST['action'] === 'store' && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $read_status = '';
     if (array_key_exists('read_status', $_POST)) {
@@ -12,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $stored_picture_book = [
-        'isbn_13' => $_POST['isbn_13'],
+        'google_books_id' => $_POST['google_books_id'],
         'five_star_rating' => (int)$_POST['five_star_rating'],
         'read_status' => $read_status,
         'summary' => $_POST['summary'],
@@ -26,4 +30,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $dbc->storePictureBook($stored_picture_book, $user_id);
     header("Location: bookshelf.php");
     exit();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $picture_book = $_POST;
+    // バリデーションする
+    $dbc = new Dbc;
+    $dbc->createPictureBook($picture_book);
+    $stored_picture_book = [
+        'google_books_id' => $picture_book['google_books_id'],
+        'picture_book_id' => '',
+        'five_star_rating' => '',
+        'read_status' => 'よみたい',
+        'summary' => ''
+    ];
+    $errors = [];
 }
+
+$title = 'よんで-Yonde-詳細登録';
+$content = __DIR__ . '/views/store_picture_book.php';
+include __DIR__ . '/views/layout.php';
