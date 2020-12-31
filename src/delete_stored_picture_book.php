3c72114f93939c857798a1c819150853106a8a7d
ryamate <iiiiiii.satenanbon@gmail.com>
2021-01-01 06:34:31 +0900

    [bug] 本棚ページの削除ボタン押すと別レコードが消える closed #62
    - modal-target に個別に Id を付与

--- src/delete_stored_picture_book.php
+++ src/delete_stored_picture_book.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/lib/db_connect.php';
 if (isset($_SESSION['id'])) {
     $login_user_id = $_SESSION['id'];
     $stored_picture_book_id = $_POST['stored_picture_book_id'];
+    var_export($stored_picture_book_id);
     $dbc = new Dbc;
     $dbc->deleteStoredPictureBook($login_user_id, $stored_picture_book_id);
     header("Location: bookshelf.php");
