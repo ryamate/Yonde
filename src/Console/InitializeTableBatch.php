ab7c70a3df59e0a2c7de74a4b0f9b292ee4c1e32
ryamate <iiiiiii.satenanbon@gmail.com>
2021-01-14 18:38:39 +0900

    [refactor] InitializeTableBatch.php クラス化 closed #5
    - ディレクトリ構成の微修正

---
+++ src/Console/InitializeTableBatch.php
@@ -0,0 +1,244 @@
+<?php
+
+/**
+ * アプリケーション開発する際に、テーブルを初期化するためのプログラムです。
+ */
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../lib/db_connect.php';
+
+class InitializeTableBatch extends Dbc
+{
+    public static function dropTable($table_name)
+    {
+        $dbh = self::dbConnect();
+        $sql = "DROP TABLE IF EXISTS $table_name;";
+        $result = $dbh->query($sql);
+        if ($result) {
+            echo 'テーブル削除完了: ' . $table_name . PHP_EOL;
+        } else {
+            exit('Error: テーブル削除失敗: ' . $table_name . PHP_EOL);
+        }
+        $dbh = null;
+    }
+
+    public static function createTable($table_name)
+    {
+        $dbh = self::dbConnect();
+        if ($table_name === 'families') {
+            $sql = <<<EOT
+            CREATE TABLE $table_name (
+                id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                created_user_id INTEGER,
+                family_name VARCHAR(100) NULL,
+                created_at TIMESTAMP NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                ) DEFAULT CHARACTER SET=utf8mb4;
+            EOT;
+        } elseif ($table_name === 'users') {
+            $sql = <<<EOT
+            CREATE TABLE $table_name (
+                id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                family_id INTEGER NULL,
+                user_name VARCHAR(100) UNIQUE,
+                email VARCHAR(100) UNIQUE,
+                password VARCHAR(100),
+                user_image_path VARCHAR(100),
+                introduction VARCHAR(1000),
+                created_at TIMESTAMP NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                ) DEFAULT CHARACTER SET=utf8mb4;
+            EOT;
+        } elseif ($table_name === 'children') {
+            $sql = <<<EOT
+            CREATE TABLE children (
+                id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                family_id INTEGER NOT NULL,
+                child_name VARCHAR(100),
+                child_birthday TIMESTAMP NULL,
+                created_at TIMESTAMP NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                FOREIGN KEY (family_id) REFERENCES families(id)
+            ) DEFAULT CHARACTER SET=utf8mb4;
+            EOT;
+        } elseif ($table_name === 'picture_books') {
+            $sql = <<<EOT
+            CREATE TABLE $table_name (
+                id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                google_books_id VARCHAR(100) UNIQUE,
+                title VARCHAR(255),
+                authors VARCHAR(255),
+                published_date VARCHAR(10),
+                thumbnail_uri VARCHAR(1000),
+                created_at TIMESTAMP NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            ) DEFAULT CHARACTER SET=utf8mb4;
+            EOT;
+        } elseif ($table_name === 'stored_picture_books') {
+            $sql = <<<EOT
+            CREATE TABLE $table_name (
+                id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                picture_book_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                five_star_rating INTEGER,
+                read_status VARCHAR(100),
+                summary VARCHAR(1000),
+                created_at TIMESTAMP NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                FOREIGN KEY (picture_book_id) REFERENCES picture_books(id),
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            ) DEFAULT CHARACTER SET=utf8mb4;
+            EOT;
+        } elseif ($table_name === 'read_records') {
+            $sql = <<<EOT
+            CREATE TABLE $table_name (
+                id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                stored_picture_book_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                memo VARCHAR(1000),
+                read_date TIMESTAMP NULL,
+                created_at TIMESTAMP NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                FOREIGN KEY (stored_picture_book_id) REFERENCES stored_picture_books(id),
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            ) DEFAULT CHARACTER SET=utf8mb4;
+            EOT;
+        }
+        $result = $dbh->query($sql);
+        if ($result) {
+            echo 'テーブル作成完了: ' . $table_name . PHP_EOL;
+        } else {
+            exit('Error: テーブル作成失敗: ' . $table_name . PHP_EOL);
+        }
+        $dbh = null;
+    }
+
+    public static function createGuestFamily()
+    {
+        $sql = <<<EOT
+        INSERT INTO families (
+            created_user_id,
+            family_name,
+            created_at
+        )VALUES (
+            1,
+            "ゲス",
+            NOW()
+        )
+        EOT;
+        $dbh = self::dbConnect();
+        $result = $dbh->query($sql);
+        if ($result) {
+            echo '追加完了: guest_family' . PHP_EOL;
+        } else {
+            exit('Error: 追加失敗: guest_family' . PHP_EOL);
+        }
+        $dbh = null;
+    }
+
+    public static function createGuestUser()
+    {
+        $sql = <<<EOT
+        INSERT INTO users (
+            family_id,
+            user_name,
+            email,
+            password,
+            user_image_path,
+            introduction,
+            created_at
+        )VALUES (
+            1,
+            "ゲスト",
+            "guest@guest",
+            "guest_user1",
+            "no_image_user_man.png",
+            "ゲストユーザーです。",
+            NOW()
+        )
+        EOT;
+        $dbh = self::dbConnect();
+        $result = $dbh->query($sql);
+        if ($result) {
+            echo '追加完了: guest_user' . PHP_EOL;
+        } else {
+            exit('Error: 追加失敗: guest_user' . PHP_EOL);
+        }
+        $dbh = null;
+    }
+
+    public static function createGuestPartner()
+    {
+        $sql = <<<EOT
+        INSERT INTO users (
+            family_id,
+            user_name,
+            email,
+            password,
+            user_image_path,
+            introduction,
+            created_at
+        )VALUES (
+            1,
+            "ゲスミ",
+            "guest@partner",
+            "guest_partner1",
+            "no_image_user_woman.png",
+            "ゲストユーザーの妻です。",
+            NOW()
+        )
+        EOT;
+        $dbh = self::dbConnect();
+        $result = $dbh->query($sql);
+        if ($result) {
+            echo '追加完了: guest_partner' . PHP_EOL;
+        } else {
+            exit('Error: 追加失敗: guest_partner' . PHP_EOL);
+        }
+        $dbh = null;
+    }
+
+    public static function createGuestChild()
+    {
+        $sql = <<<EOT
+        INSERT INTO children (
+            family_id,
+            child_name,
+            created_at
+        )VALUES (
+            1,
+            "ゲスタロウ",
+            NOW()
+        )
+        EOT;
+        $dbh = self::dbConnect();
+        $result = $dbh->query($sql);
+        if ($result) {
+            echo '追加完了: guest_child' . PHP_EOL;
+        } else {
+            exit('Error: 追加失敗: guest_child' . PHP_EOL);
+        }
+        $dbh = null;
+    }
+}
+
+InitializeTableBatch::dropTable('read_records');
+InitializeTableBatch::dropTable('stored_picture_books');
+InitializeTableBatch::dropTable('picture_books');
+InitializeTableBatch::dropTable('children');
+InitializeTableBatch::dropTable('users');
+InitializeTableBatch::dropTable('families');
+
+InitializeTableBatch::createTable('families');
+InitializeTableBatch::createTable('users');
+InitializeTableBatch::createTable('children');
+InitializeTableBatch::createTable('picture_books');
+InitializeTableBatch::createTable('stored_picture_books');
+InitializeTableBatch::createTable('read_records');
+
+InitializeTableBatch::createGuestUser();
+InitializeTableBatch::createGuestFamily();
+InitializeTableBatch::createGuestPartner();
+InitializeTableBatch::createGuestChild();
--- src/lib/db_connect.php
+++ src/lib/db_connect.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 require __DIR__ . '/../vendor/autoload.php';
 
 class Dbc
 {
     const MAX_DISPLAY_BOOKS = 10;
 
-    protected function dbConnect()
+    protected static function dbConnect(): PDO
     {
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
         $dotenv->load();
@@ -32,170 +34,6 @@ class Dbc
         return $dbh;
     }
 
-    public function getLoginUser($user)
-    {
-        if (empty($user)) {
-            exit('ユーザー情報が不正です。');
-        }
-
-        $dbh = $this->dbConnect();
-
-        $stmt = $dbh->prepare('SELECT * FROM users WHERE user_name = :user_name');
-        $stmt->bindValue(':user_name', $user['user_name'], PDO::PARAM_STR);
-
-        $stmt->execute();
-
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if (!$result) {
-            exit('ユーザー情報を取得できません。');
-        }
-        return $result;
-        $dbh = null;
-    }
-
-    public function validateUserLogin($user, $encoded_password)
-    {
-        $dbh = $this->dbConnect();
-
-        $stmt = $dbh->prepare('SELECT user_name, password FROM users WHERE user_name = :user_name AND password = :encoded_password');
-        $stmt->bindValue(':user_name', $user['user_name'], PDO::PARAM_STR);
-        $stmt->bindValue(':encoded_password', $encoded_password, PDO::PARAM_STR);
-
-        $stmt->execute();
-
-        $registered_user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        $dbh = null;
-
-        $errors = [];
-
-        if (!strlen($user['user_name'])) {
-            $errors['user_name'] = '*よんでID：入力願います。';
-        }
-
-        if (!strlen($user['password'])) {
-            $errors['password'] = '*パスワード：入力願います。';
-        }
-
-        if (!$registered_user) {
-            $errors['user'] = '*よんでID、パスワードを正しく入力願います。';
-        }
-
-        return $errors;
-    }
-
-    public function validateUserSignup($user, $file_name)
-    {
-        $dbh = $this->dbConnect();
-
-        $stmt = $dbh->prepare('SELECT email FROM users WHERE email = :user_email');
-        $stmt->bindValue(':user_email', $user['email'], PDO::PARAM_STR);
-        $stmt->execute();
-        $registered_email = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        $stmt = $dbh->prepare('SELECT user_name FROM users WHERE user_name = :user_name');
-        $stmt->bindValue(':user_name', $user['user_name'], PDO::PARAM_STR);
-        $stmt->execute();
-        $registered_user_name = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        $dbh = null;
-
-        $errors = [];
-
-        if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors['email'] = '*メールアドレス：入力された値が正しくありません。';
-        } elseif (isset($registered_email['email'])) {
-            $errors['email'] = '*メールアドレス：登録済みのメールアドレスです。';
-        }
-
-        if (!strlen($user['user_name'])) {
-            $errors['user_name'] = '*よんでID：入力願います。';
-        } elseif (strlen($user['user_name']) > 16) {
-            $errors['user_name'] = '*よんでID：16文字以内 で入力願います。';
-        } elseif (isset($registered_user_name['user_name'])) {
-            $errors['user_name'] = '*よんでID："' . $registered_user_name['user_name'] . '"は、使用されています。';
-        }
-
-        if (!preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i', $user['password'])) {
-            $errors['password'] =  '*パスワード：半角英数字をそれぞれ1文字以上含んだ8文字以上で設定願います。';
-        }
-
-        if (!empty($file_name)) {
-            $ext = substr($file_name, -3);
-            if ($ext !== 'gif' && $ext !== 'jpg' && $ext !== 'png') {
-                $errors['image'] = '* プロフィール画像：「.gif」または「.jpg」「.png」のファイルをアップロード願います。';
-            }
-        }
-
-        return $errors;
-    }
-
-    public function createUser($user)
-    {
-        $sql = <<<EOT
-        INSERT INTO users (
-            user_name,
-            email,
-            password,
-            user_image_path,
-            created_at
-        )VALUES (
-            :user_name,
-            :email,
-            :password,
-            :user_image_path,
-            NOW()
-        )
-        EOT;
-
-        $dbh = $this->dbConnect();
-        $dbh->beginTransaction();
-
-        try {
-            $stmt = $dbh->prepare($sql);
-
-            $stmt->bindValue(':user_name', $user['user_name'], PDO::PARAM_STR);
-            $stmt->bindValue(':email', $user['email'], PDO::PARAM_STR);
-            $stmt->bindValue(':password', $user['password'], PDO::PARAM_STR);
-            $stmt->bindValue(':user_image_path', $user['user_image_path'], PDO::PARAM_STR);
-
-            $stmt->execute();
-            $dbh->commit();
-            echo 'ユーザー登録完了';
-        } catch (PDOException $e) {
-            $dbh->rollBack();
-            exit($e);
-        }
-    }
-
-    public function createFamily($user)
-    {
-        $sql = <<<EOT
-        INSERT INTO families (
-            created_user_id,
-            created_at
-        )VALUES (
-            :created_user_id,
-            NOW()
-        )
-        EOT;
-        $dbh = $this->dbConnect();
-        $dbh->beginTransaction();
-
-        try {
-            $stmt = $dbh->prepare($sql);
-            $stmt->bindValue(':created_user_id', $user['id'], PDO::PARAM_INT);
-
-            $stmt->execute();
-            $dbh->commit();
-            echo '家族登録完了';
-        } catch (PDOException $e) {
-            $dbh->rollBack();
-            exit($e);
-        }
-    }
-
     public function displayStoredPictureBooks($login_user, $page)
     {
         $dbh = $this->dbConnect();
@@ -218,24 +56,6 @@ class Dbc
         return $results;
         $dbh = null;
     }
-
-    // public function listStoredPictureBooks($login_user)
-    // {
-    //     $dbh = $this->dbConnect();
-
-    //     $stmt = $dbh->prepare('SELECT s.id, s.picture_book_id, s.five_star_rating, s.read_status, s.possession, s.summary, s.created_at, s.updated_at, p.title, p.authors, p.thumbnail_uri, p.published_date FROM stored_picture_books s JOIN picture_books p ON s.picture_book_id = p.id WHERE user_id = :login_user_id');
-    //     $stmt->bindValue(':login_user_id', (int)$login_user['id'], PDO::PARAM_INT);
-
-    //     $stmt->execute();
-
-    //     $results = [];
-    //     while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    //         $results[] = $result;
-    //     }
-
-    //     return $results;
-    //     $dbh = null;
-    // }
 
     public function deleteStoredPictureBook($login_user_id, $stored_picture_book_id)
     {
