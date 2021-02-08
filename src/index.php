fafb5039052349104fcf3557578ba270dc4ba418
ryamate <iiiiiii.satenanbon@gmail.com>
2021-02-08 12:43:28 +0900

    [refactor] ルートディレクトリを public に変更する closed #95
    - public/index.php を表示するためのディレクトリ修正をあわせて実施

--- src/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-session_destroy();
-
-$title = 'よんで-Yonde-';
-$content = __DIR__ . '/views/index.php';
-include __DIR__ . '/views/layout_before_login.php';
