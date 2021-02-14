2ed352e924635fc8fe43fb434bf54a2c3e5e77d1
ryamate <iiiiiii.satenanbon@gmail.com>
2021-02-14 20:57:13 +0900

    [feat] メールアドレス変更機能追加 #115

--- src/views/users/modify_nickname.php
+++ src/views/users/modify_nickname.php
@@ -7,7 +7,7 @@
         <table class="container table table-bordered">
             <tr>
                 <th>よんでID</th>
-                <td><?= escape($login_user['user_name']) ?>　<a href="modify_user_name.php">変更はこちらから</a></td>
+                <td><?= escape($login_user['user_name']) ?>　<a href="modify_user_name.php" class="small">変更はこちらから</a></td>
             </tr>
 
             <tr>
@@ -20,14 +20,14 @@
                         <?php if (isset($errors['new_nickname'])) : ?>
                             <li class="text-danger"><?= $errors['new_nickname'] ?></li>
                         <?php endif; ?>
-                        <li>日本語もしくは英字3～16文字</li>
+                        <li>日本語もしくは英数字16文字以内</li>
                     </ul>
                 </td>
             </tr>
 
             <tr>
                 <th>メールアドレス</th>
-                <td><?= escape($login_user['email']) ?>　<a href="modify_email.php">変更はこちらから</a></td>
+                <td><?= escape($login_user['email']) ?>　<a href="modify_email.php" class="small">変更はこちらから</a></td>
             </tr>
         </table>
 
