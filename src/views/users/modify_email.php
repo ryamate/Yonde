2d1d2ebb2fcfcc7b9bec7863bab8f76082241e04
ryamate <iiiiiii.satenanbon@gmail.com>
2021-02-21 14:06:21 +0900

    [feat] 絵本棚ページのよみきかせ記録件数表示機能追加 closed #127
    - [style] 各ページのレスポンシブ対応、デザイン崩れ修正 closed #126

--- src/views/users/modify_email.php
+++ src/views/users/modify_email.php
@@ -1,47 +1,49 @@
-<div style="margin-top: 75px;">
-    <form action="modify_email.php" method="POST">
-        <div class="container">
-            <h2>メールアドレスの変更</h2>
-        </div>
+<main>
+    <div style="margin-top: 75px;">
+        <form action="modify_email.php" method="POST">
+            <div class="container">
+                <h2>メールアドレスの変更</h2>
+            </div>
 
-        <table class="container table table-bordered">
-            <tr>
-                <th>現在のメールアドレス</th>
-                <td><?= escape($login_user['email']) ?></td>
-            </tr>
+            <table class="container table table-bordered">
+                <tr>
+                    <th>現在のメールアドレス</th>
+                    <td><?= escape($login_user['email']) ?></td>
+                </tr>
 
-            <tr>
-                <th>
-                    <label for="new_email">新しいメールアドレス</label>
-                </th>
-                <td>
-                    <input type="text" placeholder="新しいメールアドレス" name="new_email" id="new_email" value="<?= escape($check_user['new_email']); ?>" maxlength="100">
-                    <ul class="text-dark small">
-                        <?php if (isset($errors['new_email'])) : ?>
-                            <li class="text-danger"><?= $errors['new_email'] ?></li>
-                        <?php endif; ?>
-                    </ul>
-                </td>
-            </tr>
+                <tr>
+                    <th>
+                        <label for="new_email">新しいメールアドレス</label>
+                    </th>
+                    <td>
+                        <input type="text" placeholder="新しいメールアドレス" name="new_email" id="new_email" value="<?= escape($check_user['new_email']); ?>" maxlength="100">
+                        <ul class="text-dark small">
+                            <?php if (isset($errors['new_email'])) : ?>
+                                <li class="text-danger"><?= $errors['new_email'] ?></li>
+                            <?php endif; ?>
+                        </ul>
+                    </td>
+                </tr>
 
-            <tr>
-                <th>
-                    <label for="password">パスワード</label>
-                </th>
-                <td>
-                    <input type="password" placeholder="パスワード" name="password" id="password" value="">
-                    <ul class="text-dark small">
-                        <?php if (isset($errors['password'])) : ?>
-                            <li class="text-danger"><?= $errors['password'] ?></li>
-                        <?php endif; ?>
-                        <li>確認のためにログインパスワードを入力してください</li>
-                    </ul>
-                </td>
-            </tr>
-        </table>
+                <tr>
+                    <th>
+                        <label for="password">パスワード</label>
+                    </th>
+                    <td>
+                        <input type="password" placeholder="パスワード" name="password" id="password" value="">
+                        <ul class="text-dark small">
+                            <?php if (isset($errors['password'])) : ?>
+                                <li class="text-danger"><?= $errors['password'] ?></li>
+                            <?php endif; ?>
+                            <li>確認のためにログインパスワードを入力してください</li>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
 
-        <div class="text-center">
-            <button type="submit" class="btn btn-sm bg-white btn-outline-primary text-decoration-none text-primary">メールアドレスを変更する</button></span>
-        </div>
-    </form>
-</div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-sm bg-white btn-outline-primary text-decoration-none text-primary">メールアドレスを変更する</button></span>
+            </div>
+        </form>
+    </div>
+</main>
--- src/views/users/profile_setting.php
+++ src/views/users/profile_setting.php
@@ -1,73 +1,68 @@
-<div style="margin-top: 75px;">
-    <form method="POST" action="profile_setting.php">
-        <div class="container">
+<main>
+    <div class="container" style="margin-top: 75px; max-width: 540px;">
+        <form method=" POST" action="profile_setting.php">
             <h2>プロフィール設定</h2>
-        </div>
+            <table class="container table table-bordered">
+                <tr>
+                    <th>よんでID</th>
+                    <td><?= escape($login_user['user_name']) ?>　<a href="modify_user_name.php" class="small">変更はこちらから</a></td>
+                </tr>
 
-        <table class="container table table-bordered">
-            <tr>
-                <th>よんでID</th>
-                <td><?= escape($login_user['user_name']) ?>　<a href="modify_user_name.php" class="small">変更はこちらから</a></td>
-            </tr>
+                <tr>
+                    <th>ニックネーム</th>
+                    <td><?= escape($login_user['nickname']) ?>　<a href="modify_nickname.php" class="small">変更はこちらから</a></td>
+                </tr>
 
-            <tr>
-                <th>ニックネーム</th>
-                <td><?= escape($login_user['nickname']) ?>　<a href="modify_nickname.php" class="small">変更はこちらから</a></td>
-            </tr>
-
-            <tr>
-                <th>プロフィール画像</th>
-                <td>
-                    <?php if ($login_user['user_icon'] !== '') : ?>
-                        <img src="../assets/images/user_icon/<?= escape($login_user['user_icon']); ?>" alt="プロフィール画像" style="width: 100px; height:100px;background-position: center center;object-fit:cover;">
-                    <?php else : ?>
-                        <i class="far fa-user-circle fa-5x text-secondary"></i>
-                    <?php endif; ?>
-                    <a href="modify_user_icon.php" class="small">変更はこちらから</a>
-                </td>
-            </tr>
-
-            <tr>
-                <th>
-                    <label for="new_introduction">自己紹介</label>
-                </th>
-                <td>
-                    <textarea name="new_introduction" id="new_introduction" class="form-control"><?= escape($login_user['introduction']); ?></textarea>
-                    <ul class="text-dark small">
-                        <?php if (isset($errors['new_introduction'])) : ?>
-                            <li class="text-danger"><?= $errors['new_introduction'] ?></li>
+                <tr>
+                    <th>プロフィール画像</th>
+                    <td>
+                        <?php if ($login_user['user_icon'] !== '') : ?>
+                            <img src="../assets/images/user_icon/<?= escape($login_user['user_icon']); ?>" alt="プロフィール画像" style="width: 100px; height:100px;background-position: center center;object-fit:cover;">
+                        <?php else : ?>
+                            <i class="far fa-user-circle fa-5x text-secondary"></i>
                         <?php endif; ?>
-                        <li>1000文字以内 / HTMLタグ使用不可</li>
-                    </ul>
+                        <a href="modify_user_icon.php" class="small">変更はこちらから</a>
+                    </td>
+                </tr>
 
-                    <button type="submit" class="btn btn-sm bg-white btn-outline-primary text-decoration-none text-primary ml-2">自己紹介文を更新する</button>
+                <tr>
+                    <th>
+                        <label for="new_introduction">自己紹介</label>
+                    </th>
+                    <td>
+                        <textarea name="new_introduction" id="new_introduction" class="form-control"><?= escape($login_user['introduction']); ?></textarea>
+                        <ul class="text-dark small">
+                            <?php if (isset($errors['new_introduction'])) : ?>
+                                <li class="text-danger"><?= $errors['new_introduction'] ?></li>
+                            <?php endif; ?>
+                            <li>1000文字以内 / HTMLタグ使用不可</li>
+                        </ul>
 
-                </td>
-            </tr>
-        </table>
-    </form>
+                        <button type="submit" class="btn btn-sm bg-white btn-outline-primary text-decoration-none text-primary ml-2">自己紹介文を更新する</button>
 
-    <form method="POST" action="/setting/profile">
-        <input type="hidden" name="_method" value="profile">
+                    </td>
+                </tr>
+            </table>
+        </form>
 
-        <div class="container">
+        <form method="POST" action="/setting/profile">
+            <input type="hidden" name="_method" value="profile">
             <h2>ファミリー設定</h2>
-        </div>
+            <table class="container table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>ファミリーネーム【機能追加予定】</th>
+                        <td>---　<a href="" class="small">変更はこちらから</a></td>
+                    </tr>
 
-        <table class="container table table-bordered">
-            <tbody>
-                <tr>
-                    <th>ファミリーネーム【機能追加予定】</th>
-                    <td>---　<a href="" class="small">変更はこちらから</a></td>
-                </tr>
+                    <tr>
+                        <th>ファミリーメンバー【機能追加予定】</th>
+                        <td>---　<a href="" class="small">変更はこちらから</a></td>
+                    </tr>
 
-                <tr>
-                    <th>ファミリーメンバー【機能追加予定】</th>
-                    <td>---　<a href="" class="small">変更はこちらから</a></td>
-                </tr>
+                </tbody>
+            </table>
+        </form>
 
-            </tbody>
-        </table>
-    </form>
-
-</div>
+    </div>
+</main>
