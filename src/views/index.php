7f9592b10a6a6b8760741b0f5803b7a796a1a9ee
ryamate <iiiiiii.satenanbon@gmail.com>
2021-02-21 22:35:41 +0900

    静的解析ツール導入 closed #123 他
    - .gitignore 編集（vendorをGit管理しない）

--- src/views/index.php
+++ src/views/index.php
@@ -1,145 +1,111 @@
-    <header style="margin-top: 60px; margin-bottom:60px;">
-        <div class="content">
-            <div class="bg-light pt-5 pb-5">
-                <div class=" container">
-                    <!-- 大 -->
-                    <div class="row">
-                        <div class="col-md-6 d-none d-xl-block">
-                            <h1><b>こどもの「よんで」が<br>
-                                    楽しみになる</b>
-                            </h1>
-                            <div class="row d-none d-md-block">
-                                <a class="col-md-4 btn btn-teal1 font-weight-bold shadow mt-2 mb-2 ml-4" href="./auth/signup.php" role="button">新規登録する</a>
-                                <a class="col-md-4 btn btn-outline-teal1 font-weight-bold shadow mt-2 mb-2" href="./auth/login.php" role="button">ログインする</a>
-                            </div>
-                        </div>
-                        <!-- 中 -->
-                        <div class="col-md-6 d-none d-md-block d-xl-none">
-                            <h3><b>こどもの「よんで」が<br>
-                                    楽しみになる</b>
-                            </h3>
-                            <div class="row d-none d-md-block">
-                                <a class="col-md-5 btn btn-teal1 font-weight-bold shadow mt-2 mb-2 ml-2" href="./auth/signup.php" role="button">新規登録する</a>
-                                <a class="col-md-5 btn btn-outline-teal1 font-weight-bold shadow mt-2 mb-2 ml-2" href="./auth/login.php" role="button">ログインする</a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 d-none d-md-block">
-                            <img src="assets/images/index_top.png" class="img-fluid position-relative" alt="..." width="80%">
-                        </div>
+<header style="margin-top: 60px; margin-bottom:60px;">
 
-                        <!-- 小 -->
-                        <div class="d-block d-md-none">
-                            <img src="assets/images/index_top.png" class="img-fluid position-relative" alt="..." width="80%" style="left:35px;">
+    <div class="content">
+        <div class="bg-light pt-5 pb-5">
+            <div class=" container">
+                <!-- 大 -->
+                <div class="row">
+                    <div class="col-md-6 d-none d-xl-block">
+                        <h1><b>こどもの「よんで」が<br>
+                                楽しみになる</b>
+                        </h1>
+                        <div class="row d-none d-md-block">
+                            <a class="col-md-4 btn btn-teal1 font-weight-bold shadow mt-2 mb-2 ml-4" href="./auth/signup.php" role="button">新規登録する</a>
+                            <a class="col-md-4 btn btn-outline-teal1 font-weight-bold shadow mt-2 mb-2" href="./auth/login.php" role="button">ログインする</a>
                         </div>
-                        <div class="text-left col-md-6 d-block d-md-none">
-                            <h4><b>こどもの「よんで」が<br>
-                                    楽しみになる</b>
-                            </h4>
+                    </div>
+                    <!-- 中 -->
+                    <div class="col-md-6 d-none d-md-block d-xl-none">
+                        <h3><b>こどもの「よんで」が<br>
+                                楽しみになる</b>
+                        </h3>
+                        <div class="row d-none d-md-block">
+                            <a class="col-md-5 btn btn-teal1 font-weight-bold shadow mt-2 mb-2 ml-2" href="./auth/signup.php" role="button">新規登録する</a>
+                            <a class="col-md-5 btn btn-outline-teal1 font-weight-bold shadow mt-2 mb-2 ml-2" href="./auth/login.php" role="button">ログインする</a>
                         </div>
-                        <div class="container d-block d-md-none">
-                            <a class="btn btn-outline-teal1 btn-block shadow mt-2 mb-2" href="./auth/login.php" role="button">ログインする</a>
-                        </div>
-                        <div class="col-lg-3 col-md-12 d-block d-md-none">
-                            <a class="btn btn-teal1 btn-block shadow mt-2 mb-2" href="./auth/signup.php" role="button">新規登録する</a>
-                        </div>
-
+                    </div>
+                    <div class="col-md-6 d-none d-md-block">
+                        <img src="assets/images/index_top.png" class="img-fluid position-relative" alt="..." width="80%">
+                    </div>
+                    <!-- 小 -->
+                    <div class="d-block d-md-none">
+                        <img src="assets/images/index_top.png" class="img-fluid position-relative" alt="..." width="80%" style="left:35px;">
+                    </div>
+                    <div class="text-left col-md-6 d-block d-md-none">
+                        <h4><b>こどもの「よんで」が<br>
+                                楽しみになる</b>
+                        </h4>
+                    </div>
+                    <div class="container d-block d-md-none">
+                        <a class="btn btn-outline-teal1 btn-block shadow mt-2 mb-2" href="./auth/login.php" role="button">ログインする</a>
+                    </div>
+                    <div class="col-lg-3 col-md-12 d-block d-md-none">
+                        <a class="btn btn-teal1 btn-block shadow mt-2 mb-2" href="./auth/signup.php" role="button">新規登録する</a>
                     </div>
                 </div>
-            </div>
-    </header>
-
-    <main>
-        <!-- 大 -->
-        <div style="margin-top: 74px;">
-            <div class="container mt-4 mb-4 text-center d-none d-md-block">
-                <h2>
-                    <b>絵本読み聞かせの記録を<br>
-                        アルバムのように管理できるアプリ<br>
-                        『よんで - Yonde - 』</b>
-                </h2>
-                <p>「よんで」はこどもの絵本読み聞かせをおねだりするお決まりのセリフ。<br><br>
-                    こどもは、たくさんの絵本を読む。こどもが絵本好きだと、同じ絵本を何度も読むこともある。<br>
-                    でも、その絵本を読んだ記録をつけることはあまりない。<br>
-                    子供は大きくなると、自分が読み聞かせてもらった本の大半は忘れてしまう。<br>
-                    だから、記録として残して家族と共有しておくと、<br>
-                    いつか「アルバム」のようなものになる。</p>
-            </div>
-            <!-- 小 -->
-            <div class="container mt-4 mb-4 text-left d-block d-md-none">
-                <h5>
-                    <b>絵本読み聞かせの記録を<br>
-                        アルバムのように管理できるアプリ<br>
-                        『よんで - Yonde - 』</b>
-                </h5>
-                <p>「よんで」はこどもの絵本読み聞かせをおねだりするお決まりのセリフ。<br><br>
-                    こどもは、たくさんの絵本を読む。こどもが絵本好きだと、同じ絵本を何度も読むこともある。<br>
-                    でも、その絵本を読んだ記録をつけることはあまりない。<br>
-                    子供は大きくなると、自分が読み聞かせてもらった本の大半は忘れてしまう。<br>
-                    だから、記録として残して家族と共有しておくと、<br>
-                    いつか「アルバム」のようなものになる。</p>
             </div>
         </div>
-        <!-- 大 -->
-        <div style="margin-top: 74px;">
-            <div class="container d-none d-md-block mt-4">
-                <div class="card-deck row">
-                    <div class="card border-0">
-                        <img src="assets/images/index_3-1.png" width="100%" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title"><b>絵本棚</b></h4>
-                            <p class="card-text">これからよみたい本、これまでよんだ本を飾ることができます。<br>
-                                画面を見せれば今どの絵本がよみたいか、こどもが教えてくれるかもしれません。
-                            </p>
-                        </div>
-                    </div>
-                    <div class="card border-0">
-                        <img src="assets/images/index_3-2.png" width="100%" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title"><b>読み聞かせ記録・管理</b></h4>
-                            <p class="card-text">絵本棚に登録した本の読み聞かせた記録を付けることができます。<br>
-                                いつ読んだかが積み上がって、「何度も読んだ」が「〇〇回も読んだ」に変わるかもしれません。
-                            </p>
-                        </div>
-                    </div>
-                    <div class="card border-0">
-                        <img src="assets/images/index_3-3-0.png" width="100%" alt="">
-                        <img src="assets/images/index_3-3.png" width="100%" alt="">
-                        <img src="assets/images/index_3-3-2.png" width="100%" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title"><b>家族と共有</b></h4>
-                            <p class="card-text">家族と絵本棚と読み聞かせ記録を共有することができます。<br>
-                                いつのまにそんな絵本を読んであげたの？と新しい発見があるかもしれません。
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+</header>
+
+<main>
+    <!-- 大 -->
+    <div style="margin-top: 74px;">
+        <div class="container mt-4 mb-4 text-center d-none d-md-block">
+            <h2>
+                <b>絵本読み聞かせの記録を<br>
+                    アルバムのように管理できるアプリ<br>
+                    『よんで - Yonde - 』</b>
+            </h2>
+            <p>「よんで」はこどもの絵本読み聞かせをおねだりするお決まりのセリフ。<br><br>
+                こどもは、たくさんの絵本を読む。こどもが絵本好きだと、同じ絵本を何度も読むこともある。<br>
+                でも、その絵本を読んだ記録をつけることはあまりない。<br>
+                子供は大きくなると、自分が読み聞かせてもらった本の大半は忘れてしまう。<br>
+                だから、記録として残して家族と共有しておくと、<br>
+                いつか「アルバム」のようなものになる。</p>
         </div>
         <!-- 小 -->
-        <div class="container d-block d-md-none mt-4">
+        <div class="container mt-4 mb-4 text-left d-block d-md-none">
+            <h5>
+                <b>絵本読み聞かせの記録を<br>
+                    アルバムのように管理できるアプリ<br>
+                    『よんで - Yonde - 』</b>
+            </h5>
+            <p>「よんで」はこどもの絵本読み聞かせをおねだりするお決まりのセリフ。<br><br>
+                こどもは、たくさんの絵本を読む。こどもが絵本好きだと、同じ絵本を何度も読むこともある。<br>
+                でも、その絵本を読んだ記録をつけることはあまりない。<br>
+                子供は大きくなると、自分が読み聞かせてもらった本の大半は忘れてしまう。<br>
+                だから、記録として残して家族と共有しておくと、<br>
+                いつか「アルバム」のようなものになる。</p>
+        </div>
+    </div>
+    <!-- 大 -->
+    <div style="margin-top: 74px;">
+        <div class="container d-none d-md-block mt-4">
             <div class="card-deck row">
                 <div class="card border-0">
-                    <a href=""><img src="assets/images/index_3-1.png" width="100%" alt=""></a>
+                    <img src="assets/images/index_3-1.png" width="100%" alt="">
                     <div class="card-body">
-                        <h5 class="card-title"><b>絵本棚</b></h5>
+                        <h4 class="card-title"><b>絵本棚</b></h4>
                         <p class="card-text">これからよみたい本、これまでよんだ本を飾ることができます。<br>
                             画面を見せれば今どの絵本がよみたいか、こどもが教えてくれるかもしれません。
                         </p>
                     </div>
                 </div>
                 <div class="card border-0">
-                    <a href=""><img src="assets/images/index_3-2.png" width="100%" alt=""></a>
+                    <img src="assets/images/index_3-2.png" width="100%" alt="">
                     <div class="card-body">
-                        <h5 class="card-title"><b>読み聞かせ記録・管理</b></h5>
+                        <h4 class="card-title"><b>読み聞かせ記録・管理</b></h4>
                         <p class="card-text">絵本棚に登録した本の読み聞かせた記録を付けることができます。<br>
                             いつ読んだかが積み上がって、「何度も読んだ」が「〇〇回も読んだ」に変わるかもしれません。
                         </p>
                     </div>
                 </div>
                 <div class="card border-0">
-                    <a href=""><img src="assets/images/index_3-3.png" width="100%" alt=""></a>
+                    <img src="assets/images/index_3-3-0.png" width="100%" alt="">
+                    <img src="assets/images/index_3-3.png" width="100%" alt="">
+                    <img src="assets/images/index_3-3-2.png" width="100%" alt="">
                     <div class="card-body">
-                        <h5 class="card-title"><b>家族と共有</b></h5>
+                        <h4 class="card-title"><b>家族と共有</b></h4>
                         <p class="card-text">家族と絵本棚と読み聞かせ記録を共有することができます。<br>
                             いつのまにそんな絵本を読んであげたの？と新しい発見があるかもしれません。
                         </p>
@@ -147,73 +113,106 @@
                 </div>
             </div>
         </div>
-
-        <!-- サービス名に込めた想い -->
-        <!-- 大 -->
-        <div style="margin-top: 74px;">
-            <div class="bg-light pt-5 pb-4">
-                <div class="container mt-4 mb-4 text-center d-none d-md-block">
-                    <h2>
-                        <b>サービス名に込めた想い</b>
-                    </h2>
-                    <p>
-                        疲れているとき、気分が乗らないときでも、<br>
-                        積み上げていく感じで読んできた記録をつけていくことで、<br>
-                        こどもに心を込めて絵本を読んであげたくなる気持ちになりたい。<br><br>
-
-                        積み上げていく感じで読んだ記録をつけていくことで、<br>
-                        同じ絵本を読んでいると飽きてしまう読む側と、<br>
-                        何度でも読んでほしいこどもの、気持ちのギャップを埋めたい。<br><br>
-
-                        こどもが成長したときに、<br>
-                        こどもに読んであげた絵本の記録が、<br>
-                        いつか見返すことができるアルバムのようなプレゼントになる。<br><br>
-
-                        みんなが実際に何度も読んでいる本を知って、<br>
-                        こどもにいい絵本とたくさん出会わせてあげたい。<br><br>
-                        絵本を買うなら何度も「よんで」と言ってもらえる心に残る絵本を選びたい。
+    </div>
+    <!-- 小 -->
+    <div class="container d-block d-md-none mt-4">
+        <div class="card-deck row">
+            <div class="card border-0">
+                <a href=""><img src="assets/images/index_3-1.png" width="100%" alt=""></a>
+                <div class="card-body">
+                    <h5 class="card-title"><b>絵本棚</b></h5>
+                    <p class="card-text">これからよみたい本、これまでよんだ本を飾ることができます。<br>
+                        画面を見せれば今どの絵本がよみたいか、こどもが教えてくれるかもしれません。
                     </p>
-                    <h1 class="text-teal1">
-                        <b>こどもの「よんで」に<br>
-                            いつでもこたえてあげる気持ちになる</b>
-                    </h1>
                 </div>
-                <!-- 小 -->
-                <div class="container mt-4 mb-4 d-block d-md-none">
-                    <h4>
-                        <b>サービス名に込めた想い</b>
-                    </h4>
-                    <p>
-                        疲れているとき、気分が乗らないときでも、<br>
-                        積み上げていく感じで読んできた記録をつけていくことで、<br>
-                        こどもに心を込めて絵本を読んであげたくなる気持ちになりたい。<br><br>
-
-                        積み上げていく感じで読んだ記録をつけていくことで、<br>
-                        同じ絵本を読んでいると飽きてしまう読む側と、<br>
-                        何度でも読んでほしいこどもの、気持ちのギャップを埋めたい。<br><br>
-
-                        こどもが成長したときに、<br>
-                        こどもに読んであげた絵本の記録が、<br>
-                        いつか見返すことができるアルバムのようなプレゼントになる。<br><br>
-
-                        みんなが実際に何度も読んでいる本を知って、<br>
-                        こどもにいい絵本とたくさん出会わせてあげたい。<br><br>
-                        絵本を買うなら何度も「よんで」と言ってもらえる心に残る絵本を選びたい。
+            </div>
+            <div class="card border-0">
+                <a href=""><img src="assets/images/index_3-2.png" width="100%" alt=""></a>
+                <div class="card-body">
+                    <h5 class="card-title"><b>読み聞かせ記録・管理</b></h5>
+                    <p class="card-text">絵本棚に登録した本の読み聞かせた記録を付けることができます。<br>
+                        いつ読んだかが積み上がって、「何度も読んだ」が「〇〇回も読んだ」に変わるかもしれません。
                     </p>
-                    <h4 class="text-teal1">
-                        <b>こどもの「よんで」に<br>
-                            いつでもこたえてあげる気持ちになる</b>
-                    </h4>
                 </div>
-                <div class="text-center d-none d-md-block">
-                    <!-- <button type="submit" class="btn btn-lg btn-teal1 shadow mt-2 mb-2">新規登録する</button> -->
-                    <a class="btn btn-teal1 btn-lg shadow mt-2 mb-2" href="./auth/signup.php" role="button">新規登録する</a>
-
-                </div>
-                <div class="container d-block d-md-none">
-                    <!-- <button type="submit" class="btn btn-teal1 btn-lg btn-block shadow mt-2 mb-2">新規登録する</button> -->
-                    <a class="btn btn-teal1 btn-block shadow mt-2 mb-2" href="./auth/signup.php" role="button">新規登録する</a>
+            </div>
+            <div class="card border-0">
+                <a href=""><img src="assets/images/index_3-3.png" width="100%" alt=""></a>
+                <div class="card-body">
+                    <h5 class="card-title"><b>家族と共有</b></h5>
+                    <p class="card-text">家族と絵本棚と読み聞かせ記録を共有することができます。<br>
+                        いつのまにそんな絵本を読んであげたの？と新しい発見があるかもしれません。
+                    </p>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
+
+    <!-- サービス名に込めた想い -->
+    <!-- 大 -->
+    <div style="margin-top: 74px;">
+        <div class="bg-light pt-5 pb-4">
+            <div class="container mt-4 mb-4 text-center d-none d-md-block">
+                <h2>
+                    <b>サービス名に込めた想い</b>
+                </h2>
+                <p>
+                    疲れているとき、気分が乗らないときでも、<br>
+                    積み上げていく感じで読んできた記録をつけていくことで、<br>
+                    こどもに心を込めて絵本を読んであげたくなる気持ちになりたい。<br><br>
+
+                    積み上げていく感じで読んだ記録をつけていくことで、<br>
+                    同じ絵本を読んでいると飽きてしまう読む側と、<br>
+                    何度でも読んでほしいこどもの、気持ちのギャップを埋めたい。<br><br>
+
+                    こどもが成長したときに、<br>
+                    こどもに読んであげた絵本の記録が、<br>
+                    いつか見返すことができるアルバムのようなプレゼントになる。<br><br>
+
+                    みんなが実際に何度も読んでいる本を知って、<br>
+                    こどもにいい絵本とたくさん出会わせてあげたい。<br><br>
+                    絵本を買うなら何度も「よんで」と言ってもらえる心に残る絵本を選びたい。
+                </p>
+                <h1 class="text-teal1">
+                    <b>こどもの「よんで」に<br>
+                        いつでもこたえてあげる気持ちになる</b>
+                </h1>
+            </div>
+            <!-- 小 -->
+            <div class="container mt-4 mb-4 d-block d-md-none">
+                <h4>
+                    <b>サービス名に込めた想い</b>
+                </h4>
+                <p>
+                    疲れているとき、気分が乗らないときでも、<br>
+                    積み上げていく感じで読んできた記録をつけていくことで、<br>
+                    こどもに心を込めて絵本を読んであげたくなる気持ちになりたい。<br><br>
+
+                    積み上げていく感じで読んだ記録をつけていくことで、<br>
+                    同じ絵本を読んでいると飽きてしまう読む側と、<br>
+                    何度でも読んでほしいこどもの、気持ちのギャップを埋めたい。<br><br>
+
+                    こどもが成長したときに、<br>
+                    こどもに読んであげた絵本の記録が、<br>
+                    いつか見返すことができるアルバムのようなプレゼントになる。<br><br>
+
+                    みんなが実際に何度も読んでいる本を知って、<br>
+                    こどもにいい絵本とたくさん出会わせてあげたい。<br><br>
+                    絵本を買うなら何度も「よんで」と言ってもらえる心に残る絵本を選びたい。
+                </p>
+                <h4 class="text-teal1">
+                    <b>こどもの「よんで」に<br>
+                        いつでもこたえてあげる気持ちになる</b>
+                </h4>
+            </div>
+            <div class="text-center d-none d-md-block">
+                <!-- <button type="submit" class="btn btn-lg btn-teal1 shadow mt-2 mb-2">新規登録する</button> -->
+                <a class="btn btn-teal1 btn-lg shadow mt-2 mb-2" href="./auth/signup.php" role="button">新規登録する</a>
+
+            </div>
+            <div class="container d-block d-md-none">
+                <!-- <button type="submit" class="btn btn-teal1 btn-lg btn-block shadow mt-2 mb-2">新規登録する</button> -->
+                <a class="btn btn-teal1 btn-block shadow mt-2 mb-2" href="./auth/signup.php" role="button">新規登録する</a>
+            </div>
+        </div>
+    </div>
+</main>
